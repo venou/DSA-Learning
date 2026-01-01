2f8e44709eebaa0958b8454dfe385a1a5cb6e825
Shiva Pandit <shivakrwpa1@gmail.com>
2026-01-01 14:52:03 +0530

    Add function to find single number in an array; implement using a hash map

--- 9_LeetCode/script.js
+++ 9_LeetCode/script.js
@@ -181,3 +181,25 @@
 
 //   return Math.max(currentCount, maxCount);
 // };
+
+// # 136 Single Numbers
+let nums = [2, 2, 1];
+
+function singleNumbers(nums) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]]++;
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]] == 1) {
+      return nums[i];
+    }
+  }
+}
+
+const result = singleNumbers(nums);
+console.log(result);
